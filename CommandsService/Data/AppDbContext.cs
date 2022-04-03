5c8647953f99b57fe387c571342997d00c4eecc6
Nathan Flynn <nflynn@czarnikow.com>
2022-04-03 13:09:11 +0100

    Starting Part 7

---
+++ CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+using CommandService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+
+        public DbSet<Platform> Platforms { get; set; }
+
+        public DbSet<Command> Commands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Platform>()
+                .HasMany(x => x.Commands)
+                .WithOne(x => x.Platform!)
+                .HasForeignKey(x => x.PlatformId);
+
+            modelBuilder
+                .Entity<Command>()
+                .HasOne(x => x.Platform)
+                .WithMany(x => x.Commands)
+                .HasForeignKey(x => x.PlatformId);
+        }
+    }
+}
--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommandService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace CommandsService
@@ -23,35 +19,44 @@ namespace CommandsService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
-            });
+            services.AddDbContext<AppDbContext>(x => x.UseInMemoryDatabase("InMen"));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddTransient<ICommandRepo, CommandRepo>();
+            services.AddSwaggerGen(
+                c =>
+                {
+                    c.SwaggerDoc(
+                        "v1",
+                        new OpenApiInfo { Title = "CommandsService", Version = "v1" }
+                    );
+                }
+            );
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommandsService v1"));
+                app.UseSwaggerUI(
+                    c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommandsService v1")
+                );
             }
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(
+                endpoints =>
+                {
+                    endpoints.MapControllers();
+                }
+            );
         }
     }
 }
